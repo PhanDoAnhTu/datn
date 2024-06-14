8941187b190fe260ce37ca1cf2709b529b0f46c0
Alukusulukaku <131008410+Alukusulukaku@users.noreply.github.com>
2024-06-15 02:33:06 +0700

    Update Category.js

--- frontend/src/pages/frontend/Home/Category.js
+++ frontend/src/pages/frontend/Home/Category.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 export default function CategorySection() {
     return (
         <div className="">
@@ -5,21 +7,21 @@ export default function CategorySection() {
                 <div className="overflow-hidden rounded-xl bg-gray-500 text-white shadow-md">
                     <div className="relative">
                         <img
-                            className="h-auto max-w-full object-cover"
+                            className="h-96 w-full max-w-full object-cover object-center brightness-90"
                             src="https://www.fjallraven.com/4932a5/globalassets/fjallraven/eu/startpage/ss24/w-10/row_new-arrivals_dt_1.jpg?width=960&mode=Min&bgcolor=fff&quality=100"
                             alt="test"
                         />
                         <div className="absolute bottom-0 m-4 grid grid-rows-2">
                             <h1 className="text-2xl font-bold text-white lg:text-3xl">
-                                Title
+                                Thời trang cho phái mạnh
                             </h1>
-                            <div className="grid grid-cols-2 gap-4">
-                                <button className="overflow-hidden rounded bg-white px-5 py-4 text-sm font-bold text-gray-800 shadow-md transition duration-200 ease-out hover:-translate-y-1 hover:text-xanthous-500 lg:px-6 lg:py-5 lg:text-sm">
-                                    Button 1
-                                </button>
-                                <button className="overflow-hidden rounded bg-white px-5 py-4 text-sm font-bold text-gray-800 shadow-md  transition duration-200 ease-out hover:-translate-y-1 hover:text-xanthous-500 lg:px-6 lg:py-5 lg:text-sm">
-                                    Button 2
-                                </button>
+                            <div className="grid w-fit grid-cols-2 gap-4">
+                                <Link
+                                    to={`/san-pham-theo-danh-muc/nam/665f2c582cc3fc5cc87b6536/ao-thun`}
+                                    className="overflow-hidden rounded bg-white px-5 py-4 text-center text-sm font-bold text-gray-800 shadow-md transition duration-200 ease-out hover:-translate-y-1 hover:text-xanthous-500 lg:px-6 lg:py-5 lg:text-sm"
+                                >
+                                    Duyệt ngay
+                                </Link>
                             </div>
                         </div>
                     </div>
@@ -27,21 +29,21 @@ export default function CategorySection() {
                 <div className="overflow-hidden rounded-xl bg-gray-500 text-white shadow-md">
                     <div className="relative">
                         <img
-                            className="h-auto max-w-full object-cover"
-                            src="https://www.fjallraven.com/4932a5/globalassets/fjallraven/eu/startpage/ss24/w-10/row_shells_dt_1.jpg?width=960&mode=Min&bgcolor=fff&quality=100"
+                            className="h-96 w-full max-w-full object-cover object-center brightness-90"
+                            src="https://www.fjellsport.no/assets/blobs/fjallraven-women-s-expedition-down-lite-jacket-56fc04ee27.jpeg?preset=tiny&dpr=2"
                             alt="test"
                         />
                         <div className="absolute bottom-0 m-4 grid grid-rows-2">
                             <h1 className="text-2xl font-bold text-white lg:text-3xl">
-                                Title
+                                Thời trang cho phái đẹp
                             </h1>
-                            <div className="grid grid-cols-2 gap-4">
-                                <button className="overflow-hidden rounded bg-white px-5 py-4 text-sm font-bold text-gray-800 shadow-md transition duration-200 ease-out hover:-translate-y-1 hover:text-xanthous-500 lg:px-6 lg:py-5 lg:text-sm">
-                                    Button 1
-                                </button>
-                                <button className="overflow-hidden rounded bg-white px-5 py-4 text-sm font-bold text-gray-800 shadow-md  transition duration-200 ease-out hover:-translate-y-1 hover:text-xanthous-500 lg:px-6 lg:py-5 lg:text-sm">
-                                    Button 2
-                                </button>
+                            <div className="grid w-fit grid-cols-2 gap-4">
+                                <Link
+                                    to={`/san-pham-theo-danh-muc/nu/665f18fe2cc3fc5cc87b64f4/ao-thun`}
+                                    className="overflow-hidden rounded bg-white px-5 py-4 text-center text-sm font-bold text-gray-800 shadow-md transition duration-200 ease-out hover:-translate-y-1 hover:text-xanthous-500 lg:px-6 lg:py-5 lg:text-sm"
+                                >
+                                    Duyệt ngay
+                                </Link>
                             </div>
                         </div>
                     </div>
